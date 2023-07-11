eb8fbaaef0bbbb9a55918e6c3026d0babe12b7fb
zuelfiekarar <zuelfiekarar@gmail.com>
2023-07-12 02:49:10 +0700

    Navbar

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./#2/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        'bg-base': '#9beae0',
+    },
+    },
+  },
+  plugins: [],
+}
+
